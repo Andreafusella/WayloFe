fd7723bddfce62d33694528f5612a11b0666183b
Andreafusella <andrea55fusella@gmail.com>
2026-05-10 10:29:38 +0200

    update

--- config/firebaseConfig.js
+++ config/firebaseConfig.js
@@ -3,18 +3,19 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { initializeAuth, getReactNativePersistence } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCBGwJ9szgs_oGBot9xmMSzFtQgJ4ad9Oc",
-  authDomain: "waylo-a46d5.firebaseapp.com",
-  projectId: "waylo-a46d5",
-  storageBucket: "waylo-a46d5.firebasestorage.app",
-  messagingSenderId: "259972367503",
-  appId: "1:259972367503:web:f7cc8cfac4b398e1239cfa"
+  apiKey: "AIzaSyDfBManCOxSWC3Xfagbkgb-EXUQe1hGj9I",
+  authDomain: "waylo-f3dc2.firebaseapp.com",
+  projectId: "waylo-f3dc2",
+  storageBucket: "waylo-f3dc2.firebasestorage.app",
+  messagingSenderId: "77126962059",
+  appId: "1:77126962059:web:1d194512068d314e803d8e",
+  measurementId: "G-RMHBKMBL04"
 };
 
 const app = initializeApp(firebaseConfig);
 
 const auth = initializeAuth(app, {
-    persistence: getReactNativePersistence(AsyncStorage)
+  persistence: getReactNativePersistence(AsyncStorage)
 });
 
 export { auth };
